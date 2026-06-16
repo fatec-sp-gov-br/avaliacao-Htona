c00218c01d69c734a9d10417f204ab862f07d23c
Vitoria Ágata Dantas de Santana <vitoriasantanafocos@gmail.com>
2026-06-16 16:25:50 -0300

    Update Jogo.java

--- src/main/java/br/edu/poo/Jogo.java
+++ src/main/java/br/edu/poo/Jogo.java
@@ -4,8 +4,35 @@ public class Jogo {
 
     public static void main(String[] args) {
 
-        // Implemente a batalha aqui
+        Personagem artemis = new Personagem("Artemis", 30, 8, 2);
+        Personagem goblin = new Personagem("Goblin", 20, 6, 1);
 
+        Pocao pequena = new Pocao("Poção Pequena", 10);
+
+        System.out.println("=== BATALHA INICIADA ===");
+
+        artemis.atacar(goblin);
+        goblin.atacar(artemis);
+
+        goblin.usarPocao(pequena);
+
+        while (artemis.estaVivo() && goblin.estaVivo()) {
+
+            artemis.atacar(goblin);
+
+            if (!goblin.estaVivo()) {
+                break;
+            }
+
+            goblin.atacar(artemis);
+        }
+
+        System.out.println("=== FIM DA BATALHA ===");
+
+        if (artemis.estaVivo()) {
+            System.out.println("Vencedor: " + artemis.getNome());
+        } else {
+            System.out.println("Vencedor: " + goblin.getNome());
+        }
     }
-
 }
